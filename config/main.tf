769f09ceb130643c288de6f11b87b6d773e019c0
SanyaTangook <champmc123@gmail.com>
2024-06-27 10:44:33 +0700

    test get data ecs service

--- config/main.tf
+++ config/main.tf
@@ -1,13 +1,19 @@
-module "api_gateway" {
-  for_each = var.data_api
-  source   = "../modules/api_geteway"
-  api_id   = each.value
-  name_api = each.key
-  domain_name = var.domain_name[each.key]
-  name_certificate = var.name_certificate
+# module "api_gateway" {
+#   for_each = var.data_api
+#   source   = "../modules/api_geteway"
+#   api_id   = each.value
+#   name_api = each.key
+#   domain_name = var.domain_name[each.key]
+#   name_certificate = var.name_certificate
   
-}
+# }
 
-output "api_gateway" {
-  value = module.api_gateway
+# output "api_gateway" {
+#   value = module.api_gateway
+# }
+
+
+data "aws_ecs_service" "example" {
+  service_name = var.service_name.service_name
+  cluster_arn  = var.service_name.cluster_arn
 }
--- config/var.tf
+++ config/var.tf
@@ -1,16 +1,24 @@
-variable "data_api" {
-  type = map(string)
-}
+# variable "data_api" {
+#   type = map(string)
+# }
 
-variable "domain_name" {
-  type = map(string)
-}
+# variable "domain_name" {
+#   type = map(string)
+# }
 
-variable "name_certificate" {
-  type = string
-}
+# variable "name_certificate" {
+#   type = string
+# }
 
 variable "region" {
   type = string
   default = "ap-southeast-1"
+}
+
+
+variable "service_name" {
+  type = object({
+    service_name = string
+    cluster_arn = string
+  })
 }
