fc9733a2d6820084c885873775d28991314ae4b9
SanyaTangook <champmc123@gmail.com>
2024-06-27 11:30:45 +0700

    delete  ecs service

--- config/main.tf
+++ config/main.tf
@@ -1,34 +1,12 @@
-# module "api_gateway" {
-#   for_each = var.data_api
-#   source   = "../modules/api_geteway"
-#   api_id   = each.value
-#   name_api = each.key
-#   domain_name = var.domain_name[each.key]
-#   name_certificate = var.name_certificate
-
-# }
-
-# output "api_gateway" {
-#   value = module.api_gateway
-# }
-
-
-data "aws_ecs_service" "example" {
-  for_each     = var.service_name
-  service_name = each.value.service_name
-  cluster_arn  = each.value.cluster_arn
+module "api_gateway" {
+  for_each = var.data_api
+  source   = "../modules/api_geteway"
+  api_id   = each.value
+  name_api = each.key
+  domain_name = var.domain_name[each.key]
+  name_certificate = var.name_certificate
 }
 
-data "aws_ecs_container_definition" "ecs-mongo" {
-  for_each        = var.container
-  task_definition = each.value.task_definition
-  container_name  = each.value.container_name
-}
-
-output "ecs" {
-  value = data.aws_ecs_service.example
-}
-
-output "container" {
-  value = data.aws_ecs_container_definition.ecs-mongo
+output "api_gateway" {
+  value = module.api_gateway
 }
--- config/var.tf
+++ config/var.tf
@@ -1,30 +1,17 @@
-# variable "data_api" {
-#   type = map(string)
-# }
+variable "data_api" {
+  type = map(string)
+}
 
-# variable "domain_name" {
-#   type = map(string)
-# }
+variable "domain_name" {
+  type = map(string)
+}
 
-# variable "name_certificate" {
-#   type = string
-# }
+variable "name_certificate" {
+  type = string
+}
 
 variable "region" {
   type    = string
   default = "ap-southeast-1"
 }
 
-variable "container" {
-  type = map(object({
-    task_definition = string
-    container_name  = string
-  }))
-}
-
-variable "service_name" {
-  type = map(object({
-    service_name = string
-    cluster_arn  = string
-  }))
-}
