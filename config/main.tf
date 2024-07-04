22068f1e083a16651a4492dc3705370469c60caf
SanyaTangook <champmc123@gmail.com>
2024-07-04 10:56:08 +0700

    Test CodeBuild

--- config/main.tf
+++ config/main.tf
@@ -1,12 +1,20 @@
-module "api_gateway" {
-  for_each = var.data_api
-  source = "../modules/api_geteway"
-  name_api = each.key
-  api_id = each.value.Id_original
-  domain_name = each.value.domain_name
-  name_certificate = var.name_certificate
+# module "api_gateway" {
+#   for_each = var.data_api
+#   source = "../modules/api_geteway"
+#   name_api = each.key
+#   api_id = each.value.Id_original
+#   domain_name = each.value.domain_name
+#   name_certificate = var.name_certificate
+# }
+
+# output "api_gateway" {
+#  value = module.api_gateway
+# }
+
+module "test" {
+  source = "../modules/pipline/codebuild"
 }
 
-output "api_gateway" {
- value = module.api_gateway
+output "name" {
+  value = module.test
 }
