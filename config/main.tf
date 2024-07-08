1bdf51eb7344ed520729a30df2ceb6246934311d
SanyaTangook <champmc123@gmail.com>
2024-07-08 11:26:29 +0700

    add Moudles pipline

--- config/main.tf
+++ config/main.tf
@@ -1,15 +1,15 @@
-# module "api_gateway" {
-#   for_each = var.data_api
-#   source = "../modules/api_geteway"
-#   name_api = each.key
-#   api_id = each.value.Id_original
-#   domain_name = each.value.domain_name
-#   name_certificate = var.name_certificate
-# }
+module "api_gateway" {
+  for_each = var.data_api
+  source = "../modules/api_geteway"
+  name_api = each.key
+  api_id = each.value.Id_original
+  domain_name = each.value.domain_name
+  name_certificate = var.name_certificate
+}
 
-# output "api_gateway" {
-#  value = module.api_gateway
-# }
+output "api_gateway" {
+ value = module.api_gateway
+}
 
 module "test" {
   source = "../modules/pipline/codebuild"
