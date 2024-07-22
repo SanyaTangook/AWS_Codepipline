f3d4cbee342d0c55e9879222a783bb69b47aff52
SanyaTangook <champmc123@gmail.com>
2024-07-22 13:59:10 +0700

    loop modules

--- modules/ECS/main.tf
+++ modules/ECS/main.tf
@@ -1,6 +1,14 @@
+resource "aws_service_discovery_http_namespace" "namespace" {
+  for_each = toset(var.ecs_cluster)
+  name        = each.key
+}
+
 resource "aws_ecs_cluster" "ecs_cluster" {
   for_each = toset(var.ecs_cluster)
   name = each.key
+  service_connect_defaults {
+    namespace = aws_service_discovery_http_namespace.namespace.arn
+  }
 }
 
 data "aws_iam_role" "IAM" {
