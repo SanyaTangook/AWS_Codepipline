d3b7cfddd017a4988a83bf75e38a7bf429ebd067
SanyaTangook <champmc123@gmail.com>
2024-07-23 09:42:34 +0700

    Test VPC

--- config/main.tf
+++ config/main.tf
@@ -20,27 +20,27 @@
 #   value = module.test
 # }
 
-module "ECR" {
-  source   = "../modules/ECR"
-  for_each = var.ECS
-  ecr_name = each.value
-}
+# module "ECR" {
+#   source   = "../modules/ECR"
+#   for_each = var.ECS
+#   ecr_name = each.value
+# }
 
-module "namespace" {
-  source = "../modules/namespace"
-  for_each = var.ECS
-  ecs_cluster = each.key
-}
+# module "namespace" {
+#   source = "../modules/namespace"
+#   for_each = var.ECS
+#   ecs_cluster = each.key
+# }
 
-module "ecr_name" {
-  source = "../modules/ECS"
-  depends_on = [ module.namespace ]
-  for_each = var.ECS
-  cluster = each.key
-  role_ecs = var.role_ecs
-  family = each.value
-  url_ecr = var.url_ecr
-}
+# module "ecr_name" {
+#   source = "../modules/ECS"
+#   depends_on = [ module.namespace ]
+#   for_each = var.ECS
+#   cluster = each.key
+#   role_ecs = var.role_ecs
+#   family = each.value
+#   url_ecr = var.url_ecr
+# }
 
 
 module "vpc" {
@@ -49,7 +49,7 @@ module "vpc" {
   name = "my-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  azs             = ["ap-southeast-1a","ap-southeast-1b", "ap-southeast-1c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
