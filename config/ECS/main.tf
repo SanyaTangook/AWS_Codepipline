a736a2c2598fbc5324ffab961c970d43243a4ad9
Sanya <93518732+SanyaTangook@users.noreply.github.com>
2024-09-20 11:00:42 +0700

    Update main.tf

--- config/ECS/main.tf
+++ config/ECS/main.tf
@@ -1,7 +1,7 @@
 module "ecs_name" {
   source    = "../../modules/ECS"
   for_each = var.container
-  role = "arn:aws:iam::446605349908:role/ecsTaskExecutionRole-prd"
+  role = "arn:aws:iam::***********:role/ecsTaskExecutionRole-prd"
   container = var.container[each.key]
 }
 
